ba769cc9f4874df5fd4074dfc7ba7f3deb98a5b6
Juan Dorado <jfdoradotr@icloud.com>
2024-12-14 15:58:42 -0600

    Create project

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
