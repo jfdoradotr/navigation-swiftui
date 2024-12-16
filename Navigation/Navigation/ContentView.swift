4f94de7cb27fd4a198241f1200d0c69d1f6fc17b
Juan Dorado <jfdoradotr@icloud.com>
2024-12-16 10:52:30 -0600

    Use placement via toolbar item to set the button position explicitly

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,14 +7,14 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     NavigationStack {
-      List(0..<100) { i in
-        Text("Row \(i)")
-      }
-      .navigationTitle("Title goes here")
-      .navigationBarTitleDisplayMode(.inline)
-      .toolbarBackground(.blue)
-      .toolbarColorScheme(.dark)
-      .toolbar(.hidden, for: .navigationBar)
+      Text("Hello, SwiftUI!")
+        .toolbar {
+          ToolbarItem(placement: .topBarLeading) {
+            Button("Tap Me") {
+              
+            }
+          }
+        }
     }
   }
 }
