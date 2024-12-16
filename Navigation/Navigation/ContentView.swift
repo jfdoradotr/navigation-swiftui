2a78fa7d6ad45b25051c9b8123ced0060b5af803
Juan Dorado <jfdoradotr@icloud.com>
2024-12-16 10:44:44 -0600

    Use the `PathStore`

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -39,27 +39,21 @@ class PathStore {
 
 struct DetailView: View {
   var number: Int
-  @Binding var path: NavigationPath
 
   var body: some View {
     NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
       .navigationTitle("Number: \(number)")
-      .toolbar {
-        Button("Home") {
-          path = NavigationPath()
-        }
-      }
   }
 }
 
 struct ContentView: View {
-  @State private var path = NavigationPath()
+  @State private var pathStore = PathStore()
 
   var body: some View {
-    NavigationStack(path: $path) {
-      DetailView(number: 0, path: $path)
+    NavigationStack(path: $pathStore.path) {
+      DetailView(number: 0)
         .navigationDestination(for: Int.self) { i in
-          DetailView(number: i, path: $path)
+          DetailView(number: i)
         }
     }
   }
