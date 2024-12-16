adb554a59d001d240e01cb8456284ce5ba4683b6
Juan Dorado <jfdoradotr@icloud.com>
2024-12-16 10:40:58 -0600

    Create a simple `PathStore` to save the path you are navigating at

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -4,6 +4,37 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+  var path: [Int] {
+    didSet {
+      save()
+    }
+  }
+
+  private let savePath = URL.documentsDirectory.appending(path: "SavePath")
+
+  init() {
+    if let data = try? Data(contentsOf: savePath) {
+      if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+        path = decoded
+        return
+      }
+    }
+
+    path = []
+  }
+
+  func save() {
+    do {
+      let data = try JSONEncoder().encode(path)
+      try data.write(to: savePath)
+    } catch {
+      print("Failed to save navigation data")
+    }
+  }
+}
+
 struct DetailView: View {
   var number: Int
   @Binding var path: NavigationPath
